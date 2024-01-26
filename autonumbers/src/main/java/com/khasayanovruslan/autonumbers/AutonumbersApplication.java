f87208923a651fbadcc0e07d5cf9b1e56424cff5
Khasayanov Ruslan <KhasayanovRuslan@yandex.ru>
2024-01-27 00:50:53 +0300

    init project

---
+++ autonumbers/src/main/java/com/khasayanovruslan/autonumbers/AutonumbersApplication.java
@@ -0,0 +1,13 @@
+package com.khasayanovruslan.autonumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonumbersApplication.class, args);
+	}
+
+}
---
+++ autonumbers/src/test/java/com/khasayanovruslan/autonumbers/AutonumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khasayanovruslan.autonumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
